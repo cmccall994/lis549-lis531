41325084a568006c352a30b9978a21ee5134f1e9
Christy M <cmccall994@gmail.com>
2019-06-06 19:42:52 -0700

    Added html for all records

---
+++ Search/tapp.js
@@ -0,0 +1,47 @@
+function renderMovies(movie_list) {
+  movie_list.sort(function(a, b) {
+    return b.revenue - a.revenue;
+  });
+  var tbody = document.querySelector(".movies tbody");
+  tbody.textContent = "";
+  for (var idx = 0; idx < movie_list.length; idx++) {
+    var movie = movie_list[idx];
+    tbody.appendChild(renderMovie(movie));
+  }
+}
+
+function renderMovie(movie) {
+  var tr = document.createElement("tr");
+  tr.appendChild(renderMovieProp(movie.title, true));
+  tr.appendChild(renderMovieProp(movie.composer));
+  tr.appendChild(renderMovieProp(movie.date));
+  return tr;
+}
+
+function renderMovieProp(content, nonNumeric) {
+  var td = document.createElement("td");
+  td.textContent = content;
+  if ((nonNumeric = true)) {
+    td.classList.add("non-numeric");
+  }
+  return td;
+}
+
+var input = document.getElementById("movie-filter");
+
+function isMovieFound(movie) {
+  var userInput = input.value;
+  var lowercase = userInput.toLowerCase();
+  var lowerTitle = movie.title.toLowerCase();
+  if (lowerTitle.indexOf(lowercase) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+input.addEventListener("input", function() {
+  var filtered_movies = SCORES.filter(isMovieFound);
+  renderMovies(filtered_movies);
+});
